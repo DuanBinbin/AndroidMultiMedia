7160e8bd06a29aae8748bfaeb9cfb91cb1c60271
DuanBinbin <binbin.duan@pisofttech.com>
2018-12-18 10:25:44 +0800

    添加音频拆分工具类

---
+++ app/src/main/java/com/db/amm/utils/AudioSplitter.java
@@ -0,0 +1,54 @@
+package com.db.amm.utils;
+
+/**
+ * @描述：     @音频拆分工具类，将Android立体声pcm的数据结构，左右声道拆分，左右声道反转
+ * @作者：     @Bin
+ * @创建时间： @2018/12/18 10:14
+ * @参考文档   @Android立体声pcm的数据结构，左右声道拆分、左右声道反转 https://blog.csdn.net/hi_ugly/article/details/80977850
+ */
+public final class AudioSplitter {
+
+    private byte[] leftData;
+    private byte[] rightData;
+
+    public byte[] getLeftData() {
+        return leftData;
+    }
+
+    public byte[] getRightData() {
+        return rightData;
+    }
+
+    /**
+     * 立体声拆分
+     * @param data 输入数据
+     */
+    public final void splitStereoPcm(byte[] data) {
+        int monoLength = data.length / 2;
+        leftData = new byte[monoLength];
+        rightData = new byte[monoLength];
+        for (int i = 0; i < monoLength; i++) {
+            if (i % 2 == 0) {
+                System.arraycopy(data, i * 2, leftData, i, 2);
+            } else {
+                System.arraycopy(data, i * 2, rightData, i - 1, 2);
+            }
+        }
+    }
+
+    /**
+     * 左右声道进行反转
+     * @param data  输入数据
+     * @return      反转后的数据
+     */
+    public final byte[] getReversedData(byte[] data) {
+        byte[] reversed = new byte[data.length];
+        for (int i = 0; i < data.length - 3; i = i + 4) {
+            reversed[i] = data[i+2];
+            reversed[i+1] = data[i+3];
+            reversed[i+2] = data[i];
+            reversed[i+3] = data[i+1];
+        }
+        return reversed;
+    }
+}
